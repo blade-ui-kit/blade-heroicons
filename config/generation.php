da55aab522a2fffdb09b5f44d57bf25eeefc7e0d
Dan <dpock32509@gmail.com>
2021-06-25 16:35:54 -0400

    Use the generator script (#19)
    * Use the generator script
    
    * Add composer generate script and move generate script to bin
    
    * undo composer normalization
    
    * Cleanup
    
    * Update for blade-icons generate bin
    
    * cleanup generation file
    
    * update composer.json
    
    * update composer.json
    
    * Bump blade icons
    
    Co-authored-by: Dries Vints <dries@vints.io>

---
+++ config/generation.php
@@ -0,0 +1,16 @@
+<?php
+
+return [
+    [
+        'source' => __DIR__.'/../node_modules/heroicons/outline',
+        'destination' => __DIR__.'/../resources/svg',
+        'output-prefix' => 'o-',
+        'safe' => true,
+    ],
+    [
+        'source' => __DIR__.'/../node_modules/heroicons/solid',
+        'destination' => __DIR__.'/../resources/svg',
+        'output-prefix' => 's-',
+        'safe' => true,
+    ],
+];
