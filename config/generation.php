cfb9bd0576f388768cffe61986fb4aa8df368016
Duong Truong <duong@truongduong.me>
2022-08-24 20:46:32 +0700

    Update heroicons v2 (#28)
    * update heroicons version 2
    
    * update tests
    
    * update generation path
    
    * Mini icons
    
    Co-authored-by: Dries Vints <dries@vints.io>

--- config/generation.php
+++ config/generation.php
@@ -2,15 +2,21 @@
 
 return [
     [
-        'source' => __DIR__.'/../node_modules/heroicons/outline',
+        'source' => __DIR__.'/../node_modules/heroicons/24/outline',
         'destination' => __DIR__.'/../resources/svg',
         'output-prefix' => 'o-',
         'safe' => true,
     ],
     [
-        'source' => __DIR__.'/../node_modules/heroicons/solid',
+        'source' => __DIR__.'/../node_modules/heroicons/24/solid',
         'destination' => __DIR__.'/../resources/svg',
         'output-prefix' => 's-',
+        'safe' => true,
+    ],
+    [
+        'source' => __DIR__.'/../node_modules/heroicons/20/solid',
+        'destination' => __DIR__.'/../resources/svg',
+        'output-prefix' => 'm-',
         'safe' => true,
     ],
 ];
